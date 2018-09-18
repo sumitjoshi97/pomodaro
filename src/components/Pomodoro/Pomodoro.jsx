e1104941845d7f41826b253ae151ab96d64087a5
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-18 13:07:07 +0530

    added buttons

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Pomodoro = props => {
-  return (
-    <div>
-      pomodoro
-    </div>
-  )
+export class Pomodoro extends Component {
+
+    handlStart = () => {
+
+    }
+
+    handleRestart = () => {
+
+    }
+
+    handleStop = () => {
+
+    }
+    render() {
+        return (
+            <div className='pomodoro'>
+                <div className="pomodoro__clock"></div>
+
+                <div className="pomodoro__actions">
+                    <button className="btn">Restart</button>
+                    
+                    <button className="btn">Start</button>
+                    
+                    <button className="btn" onPointerDown={this.props.onStop}>Stop</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Pomodoro
