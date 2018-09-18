f05d7853a88de1eff2eae03de780a9b7a6008130
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-18 12:54:41 +0530

    added logic for rendering pomodoro view ithout using react router

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
+import Pomodoro from '../Pomodoro/Pomodoro'
 import './Home.css'
 
 class Home extends Component {
   state = {
     task: '',
     work: '',
-    lunch: ''
+    lunch: '',
+    renderPomodoro: false
   }
 
+  // function to handle change in Input
   handleInput = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
-    console.log(this.state)
   }
-  
+
+  // function to toggle Pomodoro Component render
+  togglePomodoro = () => {
+    this.setState({ renderPomodoro: !this.state.renderPomodoro })
+  }
+
   render() {
-    return (
+
+    // renders home component
+    let renderView = (
       <div className='home'>
         <div className='home__input-group'>
           <label className='home__input-group__label' htmlFor="task-name">Enter task : </label>
@@ -30,9 +39,16 @@ class Home extends Component {
           <label className='home__input-group__label' htmlFor="break-time">Time for break (in minutes):</label>
           <input className='home__input-group__input' name='lunch' onChange={this.handleInput} type="text" />
         </div>
-        <button className='home__submit btn'>Submit</button>
+        <button className='home__submit btn' onPointerDown={this.togglePomodoro}>Submit</button>
       </div>
     )
+
+    // renders pomodoro component if state.renderPomodoro is true
+    if (this.state.renderPomodoro) {
+      renderView = <Pomodoro onStop={this.togglePomodoro} />
+    }
+
+    return renderView
   }
 }
 
