3b0d12560f930d72c3222b55ad152f4471e8d086
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-18 17:34:26 +0530

    functionality of timer and switching timer from work to break added

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -1,29 +1,111 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import './Pomodoro.css'
 
 export class Pomodoro extends Component {
 
-    handlStart = () => {
-
+    // state 
+    state = {
+        mode: 'work',
+        minutes: this.props.workTime,
+        seconds: 0,
+        isTimerStart: true,
+        isPaused: false,
+        isTimerOver: false
     }
 
+    componentDidMount() {
+        // starting timer as soon as <Pomodoro/> component mounts
+        this.startTimer()
+    }
+
+    componentDidUpdate() {
+        // updates the timer
+        this.startTimer()
+
+        const { mode, isTimerOver } = this.state
+
+        // check if timer is not running
+        if (isTimerOver) {
+            alert(`${mode} time is over`)
+
+            // set mode to lunch if mode == 'work' and vice-versa, satrt and respective timer
+            if (mode === 'work') {
+                this.setState(() => ({
+                    mode: 'lunch',
+                    minutes: this.props.lunchTime,
+                    isTimerStart: true,
+                    isTimerOver: false
+                }))
+            } else {
+                this.setState(() => ({
+                    mode: 'work',
+                    minutes: this.props.workTime,
+                    isTimerStart: true,
+                    isTimerOver: false
+                }))
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        // removing timer as component unmounts
+        clearTimeout(this.timer)
+    }
+
+    startTimer = () => {
+        // start the timer clock and update
+        if (this.state.isTimerStart) {
+            this.timer = setTimeout(() => {
+                if (this.state.minutes >= 0 && this.state.seconds > 0 && this.state.seconds <= 59) {
+                    this.setState({ seconds: this.state.seconds - 1 })
+                } else if (this.state.seconds === 0 && this.state.minutes > 0) {
+                    this.setState({ minutes: this.state.minutes - 1, seconds: 59 })
+                } else {
+                    this.setState({ isTimerOver: true, isTimerStart: false })
+                }
+            }, 1000)
+        }
+    }
+
+    // function to pause the timer
+    handlePause = () => {
+        this.setState(() => ({
+            isTimerStart: false,
+            isPaused: true
+        }))
+    }
+
+    // function to restart timer from mode work and initial timer count
     handleRestart = () => {
-
+        this.setState(() => ({
+            mode: 'work',
+            minutes: this.props.workTime,
+            seconds: 0,
+            isTimerStart: true,
+            isTimerOver: false,
+            isPaused: false
+        }))
     }
 
-    handleStop = () => {
-
-    }
     render() {
         return (
             <div className='pomodoro'>
-                <div className="pomodoro__clock"></div>
-
+                <div className="pomodoro__title">{this.props.taskName}</div>
+                <div className="pomodoro__clock">
+                    <div className="pomodoro__clock__text">{this.state.minutes}:
+                        <span className="pomodoro__clock__text__small">{this.state.seconds}</span>
+                    </div>
+                </div>
                 <div className="pomodoro__actions">
-                    <button className="btn">Restart</button>
-                    
-                    <button className="btn">Start</button>
-                    
-                    <button className="btn" onPointerDown={this.props.onStop}>Stop</button>
+                    {
+                        !this.state.isPaused
+                            ? <button className="btn" onPointerDown={this.handlePause}>Pause</button>
+                            : <Fragment>
+                                <button className="btn" onPointerDown={this.handleRestart}>Restart</button>
+                                <button className="btn" onPointerDown={this.props.onStop}>Stop</button>
+                            </Fragment>
+                    }
+
                 </div>
             </div>
         )
